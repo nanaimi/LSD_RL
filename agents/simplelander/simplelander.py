8d63269e4116ab70ff57b21ff2894b49e4e5e28f
nnaimi <nnaimi@student.ethz.ch>
2021-01-07 23:36:43 +0100

    minor change to simplelander.py

--- agents/simplelander/simplelander.py
+++ agents/simplelander/simplelander.py
@@ -5,15 +5,14 @@ from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
 
 # multiprocess environment
-env = make_vec_env('MYREGISTEREDENVNAME', n_envs=1)
+# example env name UnrealSearch-RealisticRoomDoor-DiscreteColor-v0
+env = make_vec_env('UnrealLand-cpptestFloorGood-DiscretePoseColor-v0', n_envs=1)
 
 # PP02 with mlp network for both actor and critic, both with two layers and 64
 # neurons each
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=25000)
-model.save("")
-
-model = PPO2.load("")
+model.save("testrun")
 
 # Enjoy trained agent
 obs = env.reset()
