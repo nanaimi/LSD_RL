3e52c37a089120936cd49ffef17a6e5afc759957
nnaimi <nnaimi@student.ethz.ch>
2021-01-08 00:29:52 +0100

    minor change to simplelander.py

--- agents/simplelander/simplelander.py
+++ agents/simplelander/simplelander.py
@@ -3,11 +3,14 @@ import gym
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
+import gym_unrealcv
+import real_lsd
 
 # multiprocess environment
 # example env name
 # UnrealLand-cpptestFloorGood-DiscretePoseColor-v0
-env = make_vec_env('UnrealSearch-RealisticRoomDoor-DiscreteColor-v0', n_envs=1)
+env = gym.make('UnrealSearch-RealisticRoomDoor-DiscreteColor-v0')
+# env = make_vec_env('UnrealSearch-RealisticRoomDoor-DiscreteColor-v0', n_envs=1)
 
 # PP02 with mlp network for both actor and critic, both with two layers and 64
 # neurons each
