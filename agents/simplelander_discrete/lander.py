neg. softmax output bug

agents/simplelander_discrete/lander.py
@@ -90,10 +90,9 @@ print(agent.model.actor[1])
 
 for name, layer in agent.model.actor.named_modules():
     if isinstance(layer, nn.ReLU) or isinstance(layer, nn.Linear) or isinstance(layer, nn.Softmax):
-        print(layer)
-        print(type(name))
+        print(name)
         idx = int(name)
-        agent.model.actor[idx].register_forward_hook(get_activation('actor_layer_{}'.format(name)))
+        agent.model.actor[idx].register_forward_hook(get_activation('actor_layer_{}'.format(idx)))
 
 
 # output = model(x)