548c3686c0bd078f89916ea5d38219ddae1928da
Nasib Naimi <nasib.n@gmail.com>
2021-02-19 03:29:53 +0800

    removed stablebaselines dependency

--- agents/simplelander_discrete/lander.py
+++ agents/simplelander_discrete/lander.py
@@ -7,9 +7,9 @@ import pickle
 import numpy as np
 import glog as log
 
-from stable_baselines import PPO2
-from stable_baselines.common import make_vec_env
-from stable_baselines.common.policies import MlpPolicy
+# from stable_baselines import PPO2
+# from stable_baselines.common import make_vec_env
+# from stable_baselines.common.policies import MlpPolicy
 
 import gym_unrealcv
 import real_lsd
