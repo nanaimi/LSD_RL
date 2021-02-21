4cccd2c6a441276306e0f7d556e8eed92d05ed2f
Nasib Naimi <nasib.n@gmail.com>
2021-02-21 20:02:36 +0800

    test on 15000 frames

--- agents/simplelander_discrete/lander.py
+++ agents/simplelander_discrete/lander.py
@@ -83,7 +83,7 @@ lr               = 3e-4
 num_steps        = 20
 mini_batch_size  = 5
 ppo_epochs       = 4
-max_frames       = 80000
+max_frames       = 15000
 
 threshold_reward = -200 # TODO update/review
 
